18c0f79e9be33eba0f8d39a34f6c9a24c4a98f0d
Hendra Dwi Saputra <hendra@usa.com>
2025-02-17 14:06:46 +0700

    Update LoginTitle to use MaterialTheme colors for improved styling

--- app/src/main/java/cloud/hendra/petshop/ui/component/login/LoginTitle.kt
+++ app/src/main/java/cloud/hendra/petshop/ui/component/login/LoginTitle.kt
@@ -1,6 +1,5 @@
 package cloud.hendra.petshop.ui.component.login
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +9,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,7 +20,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -48,13 +47,13 @@ fun LoginTitle(navController: NavHostController, index: IndexDto, viewModel: Aut
                 text = "Selamat Datang".uppercase(),
                 fontSize = 24.sp,
                 fontWeight = FontWeight.ExtraBold,
-                color = Color.Black
+                color = MaterialTheme.colorScheme.primary
             )
             Text(
                 text = index.name,
                 fontSize = 18.sp,
                 fontWeight = FontWeight.ExtraLight,
-                color = Color.Black,
+                color = MaterialTheme.colorScheme.secondary,
                 letterSpacing = 5.sp
             )
             Spacer(modifier = Modifier.height(24.dp))
